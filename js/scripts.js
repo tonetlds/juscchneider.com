582bd852c79dec66e94108552a17376b9981566a
Luciano Tonet <tonet@gmail.com>
2016-04-25 16:48:04 -0300

    Carousels home

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+
+	/**
+	 * 	BACK TO TOP
+	 */
 	if ($('.backtotop').length) {
 	    
 	    var scrollTrigger = window.outerHeight,
@@ -29,6 +33,70 @@ $(document).ready(function() {
 
 		});
 
-	}
+	};
+
+
+
+	/**
+	 * 	INSTAGRAM carousel
+	 */
+	var owl = $(".owl-carousel").owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:false,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1	            
+	        },
+	        600:{
+	            items:3	            
+	        },
+	        1000:{
+	            items:4	           	            
+	        }
+	    }
+	});	
+	
+	$('#owl-carousel-instagram-next').click(function(e) {
+		e.preventDefault();
+	    owl.trigger('next.owl.carousel');
+	})
+	$('#owl-carousel-instagram-prev').click(function(e) {
+		e.preventDefault();	  
+	    owl.trigger('prev.owl.carousel', [300]);
+	});
+
+
+
+	/**
+	 * 	PINTEREST carousel
+	 */
+	var owl2 = $("#pinterest-carousel").owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:false,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1	            
+	        },
+	        600:{
+	            items:3	            
+	        },
+	        1000:{
+	            items:4	           	            
+	        }
+	    }
+	});	
+	
+	$('#owl-carousel-pinterest-next').click(function(e) {
+		e.preventDefault();
+	    owl2.trigger('next.owl.carousel');
+	})
+	$('#owl-carousel-pinterest-prev').click(function(e) {
+		e.preventDefault();	    
+	    owl2.trigger('prev.owl.carousel', [300]);
+	})
 
 });
